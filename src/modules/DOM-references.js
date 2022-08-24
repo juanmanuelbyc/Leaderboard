ab2e3f922f7617fdb866f3d3bb7494beb515f7bf
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-24 16:12:51 -0400

    fix linters errors

--- src/modules/DOM-references.js
+++ src/modules/DOM-references.js
@@ -1,5 +1,5 @@
-export const newName = document.querySelector("#playerName");
-export const newScore = document.querySelector("#playerScore");
-export const submit = document.querySelector("#submitButton");
-export const boardContainer = document.querySelector("#scoreBoardContainer");
-export const refreshButton = document.querySelector("#Refreshbutton");
+export const newName = document.querySelector('#playerName');
+export const newScore = document.querySelector('#playerScore');
+export const submit = document.querySelector('#submitButton');
+export const boardContainer = document.querySelector('#scoreBoardContainer');
+export const refreshButton = document.querySelector('#Refreshbutton');
