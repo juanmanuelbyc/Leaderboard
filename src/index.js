0a3e1548d2df7ee27f836880c429e188ecb7971b
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-24 15:56:07 -0400

    basic html

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import {submit, refreshButton} from './modules/DOM-references.js'
+// import Scores from './modules/scoresClass.js';
+// const scores = new Scores();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   scores.printScores();
+// });
+
+submit.addEventListener('click', () => {
+});
+
+refreshButton.addEventListener('click', () => {
+});
