b43ec53c56718395236aab22c8772eaf6370285f
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-25 01:29:22 -0400

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import './style.css';
-import { submit, refreshButton } from './modules/DOM-references.js';
-// import Scores from './modules/scoresClass.js';
-// const scores = new Scores();
+import { postScore, getScores } from './modules/apiMethods.js';
+import {
+  newName,
+  newScore,
+  submitButton,
+  refreshButton,
+  boardContainer,
+} from './modules/DOM-references.js';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   scores.printScores();
-// });
+const printRow = (rowData) => {
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  td.setAttribute('class', 'data');
+  td.innerHTML = `${rowData.user}: ${rowData.score}`;
+  tr.appendChild(td);
+  boardContainer.appendChild(tr);
+};
 
-submit.addEventListener('click', () => {});
+const loadScores = async () => {
+  const getRes = await getScores();
+  getRes.forEach((result) => {
+    printRow(result);
+  });
+};
 
-refreshButton.addEventListener('click', () => {});
+const refresh = () => {
+  boardContainer.innerHTML = '';
+  loadScores();
+};
+
+const submitScore = () => {
+  const requestBody = {
+    user: newName.value,
+    score: newScore.value,
+  };
+  postScore(requestBody);
+};
+
+loadScores();
+submitButton.addEventListener('click', submitScore);
+refreshButton.addEventListener('click', refresh);
